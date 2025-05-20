f08f88db278f0aa55589c7d18a7341ec8b2ddacc
paulramirez19 <155677755+paulramirez19@users.noreply.github.com>
2025-05-20 15:00:21 -0600

    Added tf2 to sensor fusion

--- sensor_fusion/include/sensor_fusion/sensor_fusion.h
+++ sensor_fusion/include/sensor_fusion/sensor_fusion.h
@@ -7,6 +7,7 @@
 #include <queue>
 #include <vector>
 
+#include "geometry_msgs/msg/transform_stamped.hpp"
 #include "geometry_msgs/msg/pose_stamped.hpp"
 #include "geometry_msgs/msg/twist.hpp"
 #include "nav_msgs/msg/odometry.hpp"
@@ -14,6 +15,8 @@
 #include "sensor_fusion/extended_kalman_filter.h"
 #include "sensor_fusion/observation.h"
 #include "sensor_msgs/msg/laser_scan.hpp"
+#include "tf2/LinearMath/Quaternion.h"
+#include "tf2_ros/transform_broadcaster.h"
 
 namespace sensor_fusion {
 
@@ -39,6 +42,7 @@ private:
 
     rclcpp::TimerBase::SharedPtr timer_;
     rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPtr odom_pub_;
+    std::unique_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster_;
 };
 
 } // namespace sensor_fusion
