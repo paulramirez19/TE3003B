d78ca8368745ca9d2b853ed880b65cc9a796d275
paulramirez19 <155677755+paulramirez19@users.noreply.github.com>
2025-05-19 19:13:08 -0600

    Upload ROS Domain node

---
+++ filter_tuning/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'filter_tuning'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='paul',
+    maintainer_email='155677755+paulramirez19@users.noreply.github.com',
+    description='TODO: Package description',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'filter_tuning = filter_tuning.filter_tuning:main',
+        ],
+    },
+)
