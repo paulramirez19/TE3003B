95ef36502a55d30dffaf2f3937a644fe25720df6
paulramirez19 <155677755+paulramirez19@users.noreply.github.com>
2025-05-29 19:16:25 -0600

    Added more data to logs

--- filter_tuning/filter_tuning/filter_tuning.py
+++ filter_tuning/filter_tuning/filter_tuning.py
@@ -17,20 +17,22 @@ class FilterTuning(Node):
         # File opening
         self.path: str = ''
         self.odom_raw_file = open(self.path + 'odom_raw.csv', 'w')
-        self.pose_file = open(self.path + 'pose.csv', 'w')
-        self.ctrl_file = open(self.path + 'ctrl.csv', 'w')
-        self.odom_fil_file = open(self.path + 'odom_filtered.csv', 'w')
+        self.pose_file = open(self.path + 'pose_stamped.csv', 'w')
+        self.ctrl_file = open(self.path + 'cmd_vel.csv', 'w')
+        # self.odom_fil_file = open(self.path + 'odom_filtered.csv', 'w')
 
     def get_yaw_from_quaternion(self, orientation):
-        _, _, yaw = euler_from_quaternion(orientation)
+        q = [orientation.x, orientation.y, orientation.z, orientation.w]
+        _, _, yaw = euler_from_quaternion(q)
         return yaw
 
     def odom_raw_callback(self, msg):
-        self.odom_raw_file.write(f'{msg.pose.pose.position.x},{msg.pose.pose.position.y},{self.get_yaw_from_quaternion(msg.pose.pose.orientation)}')
+        self.odom_raw_file.write(
+            f'{msg.header.stamp},{msg.pose.pose.position.x},{msg.pose.pose.position.y},{self.get_yaw_from_quaternion(msg.pose.pose.orientation)}')
 
     def pose_stamped_callback(self, msg):
         self.pose_file.write(
-            f'{msg.pose.position.x},{msg.pose.position.y},{self.get_yaw_from_quaternion(msg.pose.orientation)}')
+            f'{msg.header.stamp},{msg.pose.position.x},{msg.pose.position.y},{self.get_yaw_from_quaternion(msg.pose.orientation)}')
 
     def control_callback(self, msg):
         x_dot = msg.linear.x
@@ -39,9 +41,10 @@ class FilterTuning(Node):
         self.ctrl_file.write(f'{linear_velocity},{msg.angular.z}')
 
     def odom_filtered_callback(self, msg):
-        self.odom_fil_file.write(
-            f'{msg.pose.pose.position.x},{msg.pose.pose.position.y},{self.get_yaw_from_quaternion(msg.pose.pose.orientation)},' +
-            f'{msg.pose.covariance[0]},{msg.pose.covariance[7]},{msg.pose.covariance[35]}')
+        # self.odom_fil_file.write(
+        #     f'{msg.pose.pose.position.x},{msg.pose.pose.position.y},{self.get_yaw_from_quaternion(msg.pose.pose.orientation)},' +
+        #     f'{msg.pose.covariance[0]},{msg.pose.covariance[7]},{msg.pose.covariance[35]}')
+        None
 
 def main(args=None):
     rclpy.init(args=args)
